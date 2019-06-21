1b99d6da32ec55473cbb4bde93bc90f5a605289a
NickUsov <nicky.yalta.ua@gmail.com>
2019-06-21 22:11:20 +0300

    app Emloyees with binging custom class

---
+++ 24_WPF_Bindings/24_WPF_Bindings/Employee.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24_WPF_Bindings
+{
+    public class Employee : INotifyPropertyChanged
+    {
+        string sirname;
+        int age;
+        string rank;
+        public string Sirname
+        {
+            get { return sirname; }
+            set
+            {
+                sirname = value;
+                OnPropertyChanged("Sirname");
+            }
+        }
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                age = value;
+                OnPropertyChanged("Age");
+            }
+        }
+        public string Rank
+        {
+            get { return rank; }
+            set
+            {
+                rank = value;
+                OnPropertyChanged("Rank");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ 24_WPF_Bindings/24_WPF_Bindings/Window1.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _24_WPF_Bindings
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        Employee employee;
+        public Window1(Employee employee)
+        {
+            InitializeComponent();
+            this.employee = employee;
+            DataContext = this.employee;
+        }
+
+        private void btOk_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+            Close();
+        }
+
+        private void btCancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            Close();
+        }
+    }
+}
